7e92023200c709abb41aa15b85f7957dbb9a54b7
differance1968 <differance1968@yahoo.co.jp>
2017-01-28 13:14:36 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -16,10 +18,37 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'プロフィールを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
 
   private
+  
+
+  def correct_user
+    # 編集しようとしているユーザー　＞＞＞　current_user
+    # 編集対象のユーザー　＞＞＞　　　　　　@user
+    if current_user != @user
+      redirect_to root_path
+    end
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile, :area)
   end
 end
