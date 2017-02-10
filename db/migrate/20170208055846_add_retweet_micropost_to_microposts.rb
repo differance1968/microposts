e94c55a034e39be83e9db9390b37594e28f75655
differance1968 <differance1968@yahoo.co.jp>
2017-02-10 13:10:00 +0000

    10th

--- db/migrate/20170208055846_add_retweet_micropost_to_microposts.rb
+++ db/migrate/20170208055846_add_retweet_micropost_to_microposts.rb
@@ -1,5 +1,5 @@
 class AddRetweetMicropostToMicroposts < ActiveRecord::Migration
   def change
-    add_reference :microposts, :retweet_micropost, index: true, 
+    add_reference :microposts, :retweet_micropost, index: true
   end
 end
